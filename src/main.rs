640c84ff72d4bed23fb46fff9a3955cf26f2ca8e
DutchGhost <kasper199914@gmail.com>
2018-10-04 22:26:14 +0200

    auto build zig lib

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,19 @@
 #[link(name = "hello")]
 extern {
-    fn add(a: i32, b: i32) -> i32;
     fn printing(buf: *const u8, len: usize);
 }
 
-fn main() {
+fn print<S: AsRef<str>>(s: S) {
+    let s: &str = s.as_ref();
+    
     unsafe {
-        println!("{:?}", add(2, 10));
-
-        let s = "hello";
-
         printing(s.as_ptr(), s.len());
     }
+}
+
+fn main() {
+    let s = String::from("hello");
+
+    print(&s);
 }
 
