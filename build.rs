57d97dcb54b77f5bf325c107ef3fa50c0045f6a2
DutchGhost <kasper199914@gmail.com>
2018-10-04 22:26:49 +0200

    auto build zig lib

--- build.rs
+++ build.rs
@@ -1,14 +1,23 @@
-// fn main() {
-//     println!("cargo:rustc-link-search=native=/zig");
-//     println!("cargo:rustc-link-lib=static=hello");
-// }
-
 use std::path::Path;
 use std::env;
 
+use std::process::Command;
+
+
 fn main() {
     let dir = env::var("CARGO_MANIFEST_DIR").unwrap();
-    println!("cargo:rustc-link-search=native={}", Path::new(&dir).join("zig").display());
+    let path = Path::new(&dir);
+    
+    env::set_current_dir(path.join("zig"));
+    
+    let zig_compiler = path.parent().unwrap().join("zig-0.3").join("zig.exe");
 
-    println!("cargo:rustc-link-lib=static=hello");
+    let zig_compile = Command::new(zig_compiler.to_str().expect("Could not find zig compiler"))
+        .args(&["build-lib", "hello.zig"])
+        .output()
+        .expect("Could not compile zig library");
+
+    env::set_current_dir(path);
+   
+    println!("cargo:rustc-link-search=native={}", Path::new(&dir).join("zig").display());
 }
